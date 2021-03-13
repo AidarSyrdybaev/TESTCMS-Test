2a5b60add40e6e5011d6909ec6d8b2515c13b0c3
AidarSyrdybaev <ajdarsyrydbaev@gmail.com>
2021-03-13 18:01:05 +0600

    test

--- TestCMSProject2/Startup.cs
+++ TestCMSProject2/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -29,6 +30,7 @@ namespace TestCMSProject2
 
             services.AddSwaggerGen(c =>
             {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" }); 
                 var filePath = Path.Combine(AppContext.BaseDirectory, "SimpleSwagger.xml");
                 c.IncludeXmlComments(filePath, includeControllerXmlComments: true);
             });
